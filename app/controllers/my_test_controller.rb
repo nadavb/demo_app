5598eb49684f8e1291287ff24afc90aa5d0867dd
Nadav Benedek <nadavbenedek@gmail.com>
2011-06-12 16:17:57 +0200

    Initial commit

---
+++ app/controllers/my_test_controller.rb
@@ -0,0 +1,5 @@
+class MyTestController < ApplicationController
+	def index
+		puts "Hell"
+	end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = 'b37408ce349ac25d757ddb13c3c39a392164debec3a57a1422c098503bec464133667fe537813fc0b7eadd10a685a3111b1c7b1caabebd651f850dafbc5f1a58'
